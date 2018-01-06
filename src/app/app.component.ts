8096545bd044b985903ecb552a52dd830f2a5e50
wilrees87 <will@williams-mbp.home>
2018-01-06 23:38:48 +0000

    This is the first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  public innerWidth: any;
+  public innerHeight: any;
+
+ title: string = "FOMO Live";
+
+
+  constructor() { }
+
+
+  ngOnInit() {
+    this.innerWidth = window.innerWidth - 40;
+    this.innerHeight = window.innerHeight - 100;
+  }
+
+  @HostListener('window:resize', ['$event'])
+onResize(event) {
+  this.innerWidth = window.innerWidth - 60;
+  this.innerHeight = window.innerHeight - 100;
+}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,46 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NgModule, ApplicationRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AgmCoreModule } from '@agm/core';
+import { TweetsService} from './tweets.service';
+import { AuthenticateService} from './authenticate.service';
+import { EventsService} from './events.service';
+import { ItemsService} from './items.service';
+import { MapComponent } from './map/map.component';
+import { EventsComponent } from './events/events.component';
+
+import { AgmJsMarkerClustererModule } from '@agm/js-marker-clusterer';
+
+import { DistancePipe } from './distance.pipe';
+
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MapComponent,
+    EventsComponent,
+    DistancePipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyDDEW9pD0K6S3orttT4k_xhLmg1mM9PQgI',
+      libraries: ["places"]
+    }),
+    AgmJsMarkerClustererModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [TweetsService, AuthenticateService, EventsService, ItemsService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
